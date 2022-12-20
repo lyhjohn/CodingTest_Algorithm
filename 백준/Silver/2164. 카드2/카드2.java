28b23cb818483e132889ddfe2dc0d988a8f7b82a
임요한 <dladygks506@gmail.com>
2022-12-20 23:29:21 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45572 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,28 +5,22 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
 		Queue<Integer> queue = new LinkedList<>();
-		int sign = 1;
 
 		for (int i = 1; i <= N; i++) {
 			queue.offer(i);
 		}
 
 		while (queue.size() > 1) {
-			Integer poll = queue.poll();
-
-			if (sign == -1) {
-				queue.offer(poll);
-			}
-
-			sign *= -1;
+			queue.poll();
+			queue.offer(queue.poll());
 		}
 
 		System.out.println(queue.poll());
 	}
-
 }
