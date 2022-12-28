a2d7329c6b7eca5c2658089b708d90af862fa569
임요한 <dladygks506@gmail.com>
2022-12-28 23:42:41 +0900

    [Silver II] Title: 쇠막대기, Time: 184 ms, Memory: 16316 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String input = br.readLine();
 		Stack<String> stack = new Stack<>();
 		int result = 0;
@@ -29,9 +26,6 @@ public class Main {
 				}
 			}
 		}
-
-		bw.write(result + "");
-		bw.flush();
-		bw.close();
+        System.out.println(result);
 	}
 }
