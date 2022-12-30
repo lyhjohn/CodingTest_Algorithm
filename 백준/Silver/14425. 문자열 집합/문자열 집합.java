8b46726c1d9c59cf2b163646c90f5b1ecc6a976b
임요한 <dladygks506@gmail.com>
2022-12-30 15:00:20 +0900

    [Silver III] Title: 문자열 집합, Time: 404 ms, Memory: 31688 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,15 +14,15 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		int count = 0;
-		
-		List<String> list = new ArrayList<>();
+
+		Map<String, Integer> map = new HashMap<>();
 
 		for (int i = 0; i < N; i++) {
-			list.add(br.readLine());
+			map.put(br.readLine(), i);
 		}
 
 		for (int i = 0; i < M; i++) {
-			if (list.contains(br.readLine())) {
+			if (map.containsKey(br.readLine())) {
 				count++;
 			}
 		}
