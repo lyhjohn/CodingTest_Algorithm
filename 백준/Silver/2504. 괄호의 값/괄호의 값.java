e41c4a24580820141457f346eb54b88d7ffe5429
임요한 <dladygks506@gmail.com>
2023-01-09 21:08:43 +0900

    [Silver I] Title: 괄호의 값, Time: 128 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.java
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.java
@@ -1,55 +1,58 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Main {
-	
-	static int solve(String str) {
-		int ret = 0;
-		String arr[] = str.split("");
-		if(arr.length % 2 == 1)
-			return ret;
-		
-		Stack<String> stack = new Stack<String>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+
+		int sum = 0;
+
+		String[] arr = input.split("");
+
+		if (arr.length % 2 == 1) {
+			System.out.println(sum);
+			return;
+		}
+
+
+
+		Stack<String> stack = new Stack<>();
 		int tmp = 1;
-		for(int i = 0; i<arr.length; i++) {
-			if(arr[i].equals("(")) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("(")) {
 				tmp *= 2;
 				stack.push(arr[i]);
-			} else if(arr[i].equals("[")) { 
+			} else if (arr[i].equals("[")) {
 				tmp *= 3;
 				stack.push(arr[i]);
-			} else if(arr[i].equals(")")) {
-				if(stack.isEmpty() || !stack.peek().equals("("))
-					return ret = 0;
-				if(arr[i-1].equals("("))
-					ret += tmp;
+			} else if (arr[i].equals(")")) {
+				if (stack.isEmpty() || !stack.peek().equals("(")) {
+					System.out.println(0);
+					return;
+				}
+				if (arr[i - 1].equals("(")) {
+					sum += tmp;
+				}
 				stack.pop();
 				tmp /= 2;
-			} else if(arr[i].equals("]")) {
-				if(stack.isEmpty() || !stack.peek().equals("["))
-					return ret = 0;
-				if(arr[i-1].equals("["))
-					ret += tmp;
+			} else if (arr[i].equals("]")) {
+				if (stack.isEmpty() || !stack.peek().equals("[")) {
+					System.out.println(0);
+					return;
+				}
+				if (arr[i - 1].equals("[")) {
+					sum += tmp;
+				}
 				stack.pop();
 				tmp /= 3;
 			}
 		}
-		
-		return ret;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String str = br.readLine();
-		bw.write(solve(str) + "");
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(sum);
 	}
 
 }
