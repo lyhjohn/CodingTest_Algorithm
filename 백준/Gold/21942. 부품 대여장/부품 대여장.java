a875f8e99ce6592dda7bedabbd3334d033d1e4e8
임요한 <dladygks506@gmail.com>
2023-01-30 23:40:02 +0900

    [Gold II] Title: 부품 대여장, Time: 2728 ms, Memory: 211920 KB -BaekjoonHub

--- 백준/Gold/21942. 부품 대여장/부품 대여장.java
+++ 백준/Gold/21942. 부품 대여장/부품 대여장.java
@@ -1,62 +1,72 @@
+
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        String L = st.nextToken();
-        int ld = Integer.parseInt(L.substring(0,3));
-        int lh = Integer.parseInt(L.substring(4,6));
-        int lm = Integer.parseInt(L.substring(7, L.length()));
-        int range = ld*24*60 + lh*60 + lm;
-        int F = Integer.parseInt(st.nextToken());
 
-        HashMap<String, String> map = new HashMap<>();
-        HashMap<String, Long> fin = new HashMap<>();
+	public static void main(String[] args) throws IOException, ParseException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(st.nextToken()); // 입력된 정보 개수
+		String L = st.nextToken(); // 대여기간
 
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String date = st.nextToken();
-            String time = st.nextToken();
-            String item = st.nextToken();
-            String user = st.nextToken();
-            String id = item + "_" + user;
+		int LDay = Integer.parseInt(L.substring(0, 3)); // 대여 일수
+		int LHour = Integer.parseInt(L.substring(4, 6)); // 대여 시간
+		int LMinutes = Integer.parseInt(L.substring(7)); // 대여 분
 
-            if(map.containsKey(id)) {
-                String dateStr1 = map.get(id);
-                String dateStr2 = date + " " + time;
-                SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-                long d1 = f.parse(dateStr1).getTime();
-                long d2 = f.parse(dateStr2).getTime();
+		int F = Integer.parseInt(st.nextToken()); // 벌금
 
-                long diff = ((d2-d1) / (1000*60));
-                if(diff > range) {
-                      if(fin.containsKey(user)) {
-                          fin.put(user, fin.get(user)+(diff-range)*F);
-                      } else {
-                          fin.put(user,(diff-range)*F);
-                      }
-                }
-                map.remove(id);
-            } else {
-                map.put(id, date + " " + time);
-            }
-        }
+		int limitTIme = LDay * 24 * 60 + LHour * 60 + LMinutes;
 
-        List<String> list = new ArrayList<>(fin.keySet());
-        list.sort((s1,s2) -> s1.compareTo(s2));
-        if(fin.size() <= 0){
-            System.out.println(-1);
-        } else {
-            StringBuilder sb = new StringBuilder();
-            for(String key : list) {
-                sb.append(key + " " + fin.get(key)).append("\n");
-            }
-            System.out.print(sb.toString());
-        }
-    }
+		Map<String, String> rentalMap = new HashMap<>();
+		Map<String, Long> resultMap = new HashMap<>();
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			// 입력된 정보
+			String date = st.nextToken(); // 날짜
+			String time = st.nextToken(); // 시간
+			String part = st.nextToken(); // 사람, 부품
+			String customer = st.nextToken();
+			String id = part + "_" + customer;
+
+			if (rentalMap.containsKey(id)) {
+				String rtStartDt = rentalMap.get(id);
+				String rtEndDt = date + " " + time;
+				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+				long startDt = format.parse(rtStartDt).getTime();
+				long endDt = format.parse(rtEndDt).getTime();
+
+				long difTime = (endDt - startDt) / (1000 * 60); // 분 단위의 시간차이
+
+				if (difTime > limitTIme) {
+					resultMap.put(customer, resultMap.getOrDefault(customer, 0L) + (difTime - limitTIme) * F);
+				}
+				rentalMap.remove(id);
+			} else {
+				rentalMap.put(id, date + " " + time);
+			}
+		}
+
+		if (resultMap.isEmpty()) {
+			System.out.println(-1);
+			return;
+		}
+
+		List<String> list = new ArrayList<>(resultMap.keySet());
+		list.sort(String::compareTo);
+
+		for (String key : list) {
+			sb.append(key).append(" ").append(resultMap.get(key)).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
 }
